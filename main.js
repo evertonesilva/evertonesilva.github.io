1e454a67931aba50df265cd3519e9513727d664f
Everton Evaristo da Silva <138706406+evertonesilva@users.noreply.github.com>
2024-02-29 11:21:20 -0300

    site finalizado

---
+++ main.js
@@ -0,0 +1,20 @@
+//Desafio Área da circunferência:
+
+const PI = 3.141592;
+let raio = 10;
+let resultado =  PI * raio * raio;
+console.log(`A área da circunferência é ${resultado} m2`);
+
+//--------------------------------------------------------
+
+//Desafio troca de valores:
+
+let a = 2
+let b = 5
+
+let c = b
+b = a
+a = b
+
+console.log(a)
+console.log(b)
